4a4069d4dcfd267ff098f1af26be22e9649d0d71
mrsunshine <mail@nils-dehl.de>
2022-03-03 11:35:01 +0100

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>{{ userMsg }}</h1>
+  <HelloWorld msg="Guten Morgen!" :showList="true" />
+  <HelloWorld
+    msg="Guten Abend!"
+    :showList="false"
+    @super-button-click="onSuperButtonClick"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      userMsg: '',
+    };
+  },
+
+  methods: {
+    onSuperButtonClick(counter, userObj, userName, testString) {
+      console.log('onSuperButtonClick', counter, userObj, userName, testString);
+
+      // this.userMsg = userName + ' ' + counter + ' ' + testString;
+      this.userMsg = `${userName} ${counter} ${testString}`;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,67 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>{{ msg }} {{ counter }}</h1>
+    <h3>Hat {{ fullName }} lustige Hobbys? {{ hasFunHobbys }}</h3>
+    <p>showList: {{ showList }}</p>
+
+    <button :disabled="showList" :class="cssClass" @click="onButtonClick">
+      Counter ({{ counter }})
+    </button>
+
+    <button
+      :disabled="showList"
+      v-bind:class="showList ? 'super-button' : 'dkd-button'"
+    >
+      test dkd
+    </button>
+    <button
+      :disabled="showList"
+      :class="getCssClass() + ' test2 ' + cssClass"
+      :class="cssClass"
+    >
+      test dkd
+    </button>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(hobby, index) in user.hobbys.fun" :key="index">
+        <template v-if="index"> {{ index + 1 }}: {{ hobby }} </template>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
+
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li v-for="(value, key) in user" :key="key">
+        <template v-if="typeof value === 'object'">
+          <ul>
+            <li v-for="(valueChild, keyChild) in value" :key="keyChild">
+              {{ valueChild }}
+            </li>
+          </ul>
+        </template>
+        {{ value }}
+      </li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+
+    <ul v-if="showList">
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+          >eslint</a
+        >
+      </li>
     </ul>
+    <pre v-else-if="user.name === 'Niels'">
+      {{ user }}
+    </pre>
+    <p v-else>Worst case</p>
   </div>
 </template>
 
@@ -34,9 +69,69 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+    showList: {
+      type: Boolean,
+      default: () => true,
+    },
+  },
+  created() {
+    console.log('created', this.user);
+  },
+  mounted() {
+    console.log('mounted Hurra hurra das DOM ist da ;-)');
+  },
+  data() {
+    return {
+      counter: 1,
+      cssClass: 'super-button',
+      user: {
+        name: 'Nils',
+        nachname: 'Dehl',
+        hobbys: {
+          fun: ['vue', 'Humor', 'Minigolf'],
+          sport: 'crossfit',
+        },
+      },
+    };
+  },
+  watch: {
+    counter(newValue, oldValue) {
+      console.log('counter', newValue, oldValue);
+    },
+    user: {
+      deep: true,
+      handler(newValue, oldValue) {
+        console.log('user', newValue, oldValue);
+      },
+    },
+  },
+  computed: {
+    fullName() {
+      return this.user.name + ' ' + this.user.nachname;
+    },
+    hasFunHobbys() {
+      return this.user.hobbys.fun.length ? 'JA hat er' : ' Nein :-(';
+    },
+  },
+  methods: {
+    onButtonClick() {
+      this.counter++;
+      this.user.hobbys.fun.push('vue');
+
+      this.$emit(
+        'super-button-click',
+        this.counter,
+        this.user,
+        this.user.name,
+        'test'
+      );
+    },
+    getCssClass() {
+      return this.showList ? 'super-button' : 'dkd-button';
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -44,15 +139,16 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
+}
+
+.super-button {
+  background-color: red;
+}
+
+.dkd-button {
+  background-color: green;
 }
 </style>
